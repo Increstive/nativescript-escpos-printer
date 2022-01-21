467a9f235d7db3fbd4a8d2145bf46c7e2d98e471
Nutchapon Sukprasitpredee <nutchapon.sukp@gmail.com>
2022-01-21 23:01:57 +0700

    Android platform source

---
+++ platform-src/android/ZJPrinter/lib/src/main/java/th/co/increstive/ns/zj_printer/EventHandler.java
@@ -0,0 +1,5 @@
+package th.co.increstive.ns.zj_printer;
+
+public interface EventHandler {
+    public void handleEvent(int type, int arg1, String arg2);
+}
---
+++ platform-src/android/ZJPrinter/lib/src/main/java/th/co/increstive/ns/zj_printer/PrinterDevice.java
@@ -0,0 +1,12 @@
+package th.co.increstive.ns.zj_printer;
+
+public class PrinterDevice {
+
+    public String name;
+    public String address;
+
+    public PrinterDevice(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
