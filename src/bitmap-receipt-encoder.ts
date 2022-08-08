915324df0aed96087147e8cc1f2314c29bae0c2d
Nutchapon Sukprasitpredee <nutchapon.sukp@gmail.com>
2022-08-08 22:48:24 +0700

    Refactor code, Text config

--- src/bitmap-receipt-encoder.ts
+++ src/bitmap-receipt-encoder.ts
@@ -5,8 +5,17 @@ export interface CanvasDrawQueue {
     line: string,
     x: number,
     y: number,
-    fontSize: number;
-    align: CanvasTextAlign,
+    config: TextConfig;
+}
+
+export type TextStyle = 'normal' | 'italic' | 'oblique';
+export type TextWeight = 'normal' | 'bold';
+
+export interface TextConfig {
+    size: number;
+    align: CanvasTextAlign;
+    style: TextStyle;
+    weight: TextWeight
 }
 
 export class BitmapReceiptEncoder {
@@ -17,6 +26,14 @@ export class BitmapReceiptEncoder {
 
     private drawQueue: CanvasDrawQueue[] = [];
     private lineCursorY = 0;
+
+    // Config
+    private textConfig: TextConfig = {
+        size: 24,
+        align: 'left',
+        style: 'normal',
+        weight: 'normal'
+    }
 
     public get width() {
         return this.charWidth * 8
@@ -36,8 +53,6 @@ export class BitmapReceiptEncoder {
         text: string,
         y: number,
         maxWidth: number,
-        fontSize: number,
-        align: CanvasTextAlign,
         isIncrementHeight = true
     ) {
         let currentY = y;
@@ -45,7 +60,7 @@ export class BitmapReceiptEncoder {
         var width = 0, i, j;
         var result = '';
 
-        this.context.font = `${fontSize}px sans-serif`;
+        this.context.font = `${this.textConfig.style} normal ${this.textConfig.weight} ${this.textConfig.size}px sans-serif`;
 
         while (text.length) {
             for (i = text.length; this.context.measureText(text.substring(0, i)).width > maxWidth; i--);
@@ -55,23 +70,28 @@ export class BitmapReceiptEncoder {
             if (i !== text.length)
                 for (j = 0; result.indexOf(" ", j) !== -1; j = result.indexOf(" ", j) + 1);
 
-            lines.push(result.substr(0, j || result.length));
-            const line = result.substr(0, j || result.length);
-            currentY += fontSize;
+            lines.push(result.substring(0, j || result.length));
+            const line = result.substring(0, j || result.length);
+            currentY += this.textConfig.size;
 
             if (isIncrementHeight) {
-                this.lineCursorY += fontSize;
+                this.lineCursorY += this.textConfig.size;
             }
 
             let x = 0;
 
-            if (align === 'center') {
+            if (this.textConfig.align === 'center') {
                 x = this.width / 2;
-            } else if (align === 'right') {
+            } else if (this.textConfig.align === 'right') {
                 x = this.width;
             }
 
-            this.drawQueue.push({ line, x, y: currentY, align, fontSize })
+            this.drawQueue.push({
+                line,
+                x,
+                y: currentY,
+                config: Object.assign({}, this.textConfig),
+            });
 
             width = Math.max(width, this.context.measureText(lines[lines.length - 1]).width);
             text = text.substring(lines[lines.length - 1].length, text.length);
@@ -80,23 +100,59 @@ export class BitmapReceiptEncoder {
         return this;
     }
 
-    public pair(leftText: string, rightText: string) {
+    public pair(leftText: string, rightText: string, leftWidth: number = 0.66) {
         const tempCursorY = this.lineCursorY;
-        this.wrapText(leftText, tempCursorY, this.width * 0.66, 24, 'left')
-        this.wrapText(rightText, tempCursorY, this.width, 24, 'right', false)
+        return this.textAlign('left')
+            .wrapText(leftText, tempCursorY, this.width * leftWidth)
+            .textAlign('right')
+            .wrapText(rightText, tempCursorY, this.width, false);
+    }
+
+    public seperator(seperator = '-') {
+        const tempConfig = this.textConfig;
+        this.textAlign('left')
+            .textSize(24)
+            .textStyle('normal')
+            .textWeight('normal')
+            .wrapText(seperator.repeat(this.charWidth), this.lineCursorY, this.width)
+        this.textConfig = tempConfig;
         return this;
     }
 
-    public seperator(seperator = '-', fontSize = 24) {
-        this.wrapText(seperator.repeat(this.charWidth), this.lineCursorY, this.width, fontSize, 'left')
+    public line(text: string) {
+        return this.wrapText(text, this.lineCursorY, this.width);
+    }
+
+    // Config
+
+    public textSize(size: number) {
+        this.textConfig.size = size;
+        return this;
+    }
+
+    public textAlign(align: CanvasTextAlign) {
+        this.textConfig.align = align;
+        return this;
+    }
+
+    public textStyle(style: TextStyle) {
+        this.textConfig.style = style;
+        return this;
+    }
+
+    public textWeight(weight: TextWeight) {
+        this.textConfig.weight = weight;
         return this;
     }
 
     public draw() {
+        const r = this.lineCursorY % 8;
+        this.lineCursorY += 8 + r;
+        console.log(this.lineCursorY, this.lineCursorY % 8);
         this.canvas.height = this.lineCursorY;
         this.drawQueue.forEach(i => {
-            this.context.font = `${i.fontSize}px sans-serif`;
-            this.context.textAlign = i.align;
+            this.context.font = `${i.config.style} normal ${i.config.weight} ${i.config.size}px sans-serif`;
+            this.context.textAlign = i.config.align;
             this.context.fillText(i.line, i.x, i.y);
         });
         return { canvas: this.canvas, width: this.width, height: this.lineCursorY };
--- src/test.ts
+++ src/test.ts
@@ -10,113 +10,19 @@ function getEncoder(codepage: 'cp874' | 'unicode' = 'cp874') {
             'cp874': 0xff,
             'unicode': 0xff,
         },
-        // imageMode: 'raster',
+        imageMode: 'raster',
     };
     const encoder = new EscPosEncoder(option)
     return encoder.initialize()
         .codepage(codepage);
 }
 
-let linesHeight = 0;
-const drawQueue = [];
-
-function wrapText(ctx, text, y, maxWidth, fontSize, align, isIncrementHeight = true) {
-    let currentY = y;
-    var lines = new Array();
-    var width = 0, i, j;
-    var result;
-
-    ctx.fontSize = `${fontSize}px sans-serif`;
-
-    // Start calculation
-    while (text.length) {
-        for (i = text.length; ctx.measureText(text.substr(0, i)).width > maxWidth; i--);
-
-        result = text.substr(0, i);
-
-        if (i !== text.length)
-            for (j = 0; result.indexOf(" ", j) !== -1; j = result.indexOf(" ", j) + 1);
-
-        lines.push(result.substr(0, j || result.length));
-        const line = result.substr(0, j || result.length);
-        currentY += 24;
-
-        if (isIncrementHeight) {
-            linesHeight += 24;
-        }
-
-        let x = 0;
-
-        if (align === 'center') {
-            x = maxWidth / 2;
-        } else if (align === 'right') {
-            x = maxWidth;
-        }
-
-        console.log(line)
-        drawQueue.push({ line, x, y: currentY, align, fontSize })
-
-        width = Math.max(width, ctx.measureText(lines[lines.length - 1]).width);
-        text = text.substr(lines[lines.length - 1].length, text.length);
-    }
-
-}
-
-function pair(leftText, rightText, currentLinesHeight) {
-    const tempHeight = currentLinesHeight;
-    wrapText(ctx, leftText, tempHeight, width * 0.66, 24, 'left')
-    wrapText(ctx, rightText, tempHeight, width, 24, 'right', false)
-}
-
-function seperator(linesHeight, width) {
-    wrapText(ctx, '-'.repeat(charWidth), linesHeight, width, 24, 'left')
-}
-
-const charWidth = 46;
-const width = charWidth * 8;
-const height = 20 * 8;
-
-import { createCanvas } from 'canvas';
-const canvas = createCanvas(width, height)
-const ctx = canvas.getContext('2d')
-
-ctx.textBaseline = "top";
-ctx.font = '24px sans-serif'
-
-wrapText(ctx, 'MENUKA MANAGER', 0, width, 32, 'center')
-seperator(linesHeight, width)
-pair('Menu', 'Price', linesHeight);
-seperator(linesHeight, width)
-pair('แทงโก้โปรโมชั่นเอาท์ดอร์ผ้าห่ม', '100.00', linesHeight);
-pair('แทงโก้โปรโมชั่นเอาท์ดอร์ผ้าห่ม', '100.00', linesHeight);
-pair('แทงโก้โปรโมชั่นเอาท์ดอร์ผ้าห่ม', '100.00', linesHeight);
-
-canvas.height = linesHeight;
-
-ctx.font = '24px sans-serif'
-
-
-console.log(drawQueue);
-drawQueue.forEach(i => {
-    ctx.font = `${i.fontSize}px sans-serif`;
-    ctx.textAlign = i.align;
-    ctx.fillText(i.line, i.x, i.y);
-});
-
-const helloData = getEncoder('unicode')
-    .image(canvas, width, linesHeight)
-    .encode();
-
-const hexStr = Array.from(helloData)
-    .map(i => i.toString(16).padStart(2, '0'))
-    .join(' ');
-
-console.log(hexStr);
-
-// -------------------------------------
-
 const encoder = BitmapReceiptEncoder.create()
-    .wrapText('MENUKA MANAGER', 0, width, 32, 'center')
+    .textSize(32)
+    .textAlign('center')
+    .textWeight('bold')
+    .line('MENUKA MANAGER')
+    .textSize(24)
     .seperator()
     .pair('Menu', 'Price')
     .seperator()
@@ -127,6 +33,16 @@ const encoder = BitmapReceiptEncoder.create()
 
 // -------------------------------------
 
+const helloData = getEncoder('unicode')
+    .image(encoder.canvas, encoder.width, encoder.height)
+    .encode();
+
+const hexStr = Array.from(helloData)
+    .map(i => i.toString(16).padStart(2, '0'))
+    .join(' ');
+
+console.log(hexStr);
+
 
 function exportCanvas(canvas, fileName: string) {
     const out = fs.createWriteStream(__dirname + '/' + fileName)
@@ -135,5 +51,4 @@ function exportCanvas(canvas, fileName: string) {
     out.on('finish', () => console.log('The PNG file was created.'))
 }
 
-exportCanvas(canvas, '1.png');
 exportCanvas(encoder.canvas, '2.png');
